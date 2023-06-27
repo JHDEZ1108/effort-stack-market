3bc606fbb2006e69042b6acdc2fd33a942923587
Josué Hernández <joheca1998@outlook.com>
2023-06-27 16:46:18 -0600

    Agregando animaciones a mis productos

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Typography, Button, useTheme, Box } from '@mui/material';
+import { motion } from 'framer-motion';
 
 function Product({ product, handleAddToCart }) {
   const theme = useTheme();
@@ -9,51 +10,57 @@ function Product({ product, handleAddToCart }) {
   const primaryLight = theme.palette.primary.light;
 
   return (
-    <Grid container alignItems="center" sx={{ backgroundColor: defaultB, maxWidth: 450, }}>
-      <Grid item xs={12}>
-        <Box sx={{ maxHeight: 300, display: 'flex', justifyContent: 'center' }}>
-          <img
-            src={product.attributes.image.data[0].attributes.url}
-            alt={product.attributes.title}
-            style={{ width: '100%', objectFit: 'cover' }}
-          />
-        </Box>
-      </Grid>
-      <Grid item xs={12} sx={{ height: '100%' }}>
-        <Grid container direction="column" sx={{ backgroundColor: defaultA, height: '100%' }}>
-          <Box sx={{ p: 3 }}>
-            <Grid item sx={{ fontWeight: 'bold' }}>
-              <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-                {product.attributes.title}
-              </Typography>
-            </Grid>
-            <Grid item sx={{ pt: 1 }}>
-              <Typography variant="subtitle1">{`$${product.attributes.price}`}</Typography>
-            </Grid>
-            <Grid item sx={{ pt: 2 }}>
-              <Typography variant="body1">{product.attributes.description}</Typography>
-            </Grid>
-            <Grid item sx={{ pt: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <Button
-                variant="contained"
-                onClick={() => handleAddToCart(product)}
-                sx={{
-                  fontWeight: 'bold',
-                  backgroundColor: primaryMain,
-                  transition: 'transform .2s',
-                  '&:hover': {
-                    backgroundColor: primaryLight,
-                    transform: 'scale(1.1)',
-                  },
-                }}
-              >
-                Add to cart
-              </Button>
-            </Grid>
+    <motion.div
+      initial={{ scale: 0.7, opacity: 0 }} 
+      animate={{ scale: 1, opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Grid container alignItems="center" sx={{ backgroundColor: defaultB, maxWidth: 450, }}>
+        <Grid item xs={12}>
+          <Box sx={{ maxHeight: 300, display: 'flex', justifyContent: 'center' }}>
+            <img
+              src={product.attributes.image.data[0].attributes.url}
+              alt={product.attributes.title}
+              style={{ width: '100%', objectFit: 'cover' }}
+            />
           </Box>
         </Grid>
+        <Grid item xs={12} sx={{ height: '100%' }}>
+          <Grid container direction="column" sx={{ backgroundColor: defaultA, height: '100%' }}>
+            <Box sx={{ p: 3 }}>
+              <Grid item sx={{ fontWeight: 'bold' }}>
+                <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+                  {product.attributes.title}
+                </Typography>
+              </Grid>
+              <Grid item sx={{ pt: 1 }}>
+                <Typography variant="subtitle1">{`$${product.attributes.price}`}</Typography>
+              </Grid>
+              <Grid item sx={{ pt: 2 }}>
+                <Typography variant="body1">{product.attributes.description}</Typography>
+              </Grid>
+              <Grid item sx={{ pt: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Button
+                  variant="contained"
+                  onClick={() => handleAddToCart(product)}
+                  sx={{
+                    fontWeight: 'bold',
+                    backgroundColor: primaryMain,
+                    transition: 'transform .2s',
+                    '&:hover': {
+                      backgroundColor: primaryLight,
+                      transform: 'scale(1.1)',
+                    },
+                  }}
+                >
+                  Add to cart
+                </Button>
+              </Grid>
+            </Box>
+          </Grid>
+        </Grid>
       </Grid>
-    </Grid>
+    </motion.div>
   );
 }
 
