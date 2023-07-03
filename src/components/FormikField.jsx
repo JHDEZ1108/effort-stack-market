fda5ee41a0fb0a1433c057e06e69f4aa95a85867
Josué Hernández <joheca1998@outlook.com>
2023-07-03 09:01:16 -0600

    Agregando estilos para error

--- src/components/FormikField.jsx
+++ src/components/FormikField.jsx
@@ -1,19 +1,36 @@
 import React from "react";
-import { ErrorMessage, Field } from "formik";
-import { TextField, Box } from "@mui/material";
+import { ErrorMessage, Field, useFormikContext } from "formik";
+import { TextField, Box, useTheme } from "@mui/material";
 
 function FormikField ({ name, label, type = "text", required = false}) {
+  const theme = useTheme();
+  const errorColor = theme.palette.neutral.warning;
+  const { errors, touched } = useFormikContext();
+  const hasError = touched[name] && errors[name];
+
+  
   return (
     <Box>
       <Field
         helperText={<ErrorMessage name={name} />}
         required={required}
-        autoComplete="off"
         as={TextField}
         label={label}
         name={name}
         type={type}
+        variant="outlined"
         fullWidth
+        sx={{
+          "& .MuiOutlinedInput-root": {
+            borderColor: hasError ? errorColor : undefined,
+          },
+          "& .MuiFormLabel-root": {
+            color: hasError ? errorColor : undefined,
+          },
+          "& .MuiFormHelperText-root": {
+            color: hasError ? errorColor : undefined,
+          },
+        }}
       />
     </Box>
   );
